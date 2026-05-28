d2494470e2007e0c956e70c92ce934f907e9a7a6
Sukeshanee <zende.sukeshanee@kgce.edu.in>
2026-05-28 10:27:54 +0000

    Created New report

---
+++ sku_reports/sku_reports/report/urd_purchase_report/urd_purchase_report.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2026, Sukku and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["URD PURCHASE REPORT"] = {
+
+    filters: [
+        {
+            fieldname: "from_date",
+            label: "From Date",
+            fieldtype: "Date",
+            default: frappe.datetime.get_today(),
+            reqd: 1
+        },
+        {
+            fieldname: "to_date",
+            label: "To Date",
+            fieldtype: "Date",
+            default: frappe.datetime.get_today(),
+            reqd: 1
+        },
+        {
+            fieldname: "client_name",
+            label: "Customer",
+            fieldtype: "Link",
+            options: "Customer"
+        }
+    ],
+
+    formatter: function(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+
+        // Highlight total row
+        if (data && data.customer_id === "TOTAL") {
+            value = `<b>${value}</b>`;
+        }
+
+        return value;
+    }
+};
---
+++ sku_reports/sku_reports/report/urd_purchase_report/urd_purchase_report.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2026, Sukku and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe
+from frappe.utils import flt, nowdate
+
+def execute(filters=None):
+    filters = filters or {}
+
+    if not filters.get("from_date"):
+        filters["from_date"] = nowdate()
+    if not filters.get("to_date"):
+        filters["to_date"] = nowdate()
+
+    columns = get_columns()
+    data = []
+
+    # -------------------------
+    # SECTION: URD PURCHASE REPORT
+    # -------------------------
+    rows = get_urd_rows(filters)
+
+    for row in rows:
+        data.append(row)
+
+    # -------------------------
+    # TOTAL ROW
+    # -------------------------
+    data.append({})
+    total_amount = sum(flt(d.get("amount")) for d in rows)
+
+    data.append({
+        "customer_id": "TOTAL",
+        "amount":      total_amount
+    })
+
+    return columns, data
+
+
+# -----------------------------
+# COLUMNS
+# -----------------------------
+def get_columns():
+    return [
+        {"label": "Date",        "fieldname": "date",        "fieldtype": "Date",    "width": 140},
+        {"label": "Customer ID", "fieldname": "customer_id", "fieldtype": "Data",    "width": 180},
+        {"label": "CN No",       "fieldname": "cn_no",       "fieldtype": "Data",    "width": 150},
+        {"label": "Metal",       "fieldname": "metal",       "fieldtype": "Data",    "width": 120},
+        {"label": "Gram",        "fieldname": "gram",        "fieldtype": "Float",   "width": 100},
+        {"label": "Amount",      "fieldname": "amount",      "fieldtype": "Currency","width": 120},
+    ]
+
+
+# -----------------------------
+# URD PURCHASE ROWS
+# (field mapping to be added later)
+# -----------------------------
+def get_urd_rows(filters):
+    # TODO: map fields from POS doctype
+    return []
