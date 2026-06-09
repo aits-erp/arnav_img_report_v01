e68e3d1b171cf8dbf7455ab16becccc79a57a3ae
Komal Patil <komalp2205@gmail.com>
2026-06-09 08:59:49 +0000

    removed gram column, added gross_wt, net_wt, purity columns

--- sku_reports/sku_reports/report/urd_purchase_report/urd_purchase_report.js
+++ sku_reports/sku_reports/report/urd_purchase_report/urd_purchase_report.js
@@ -1,3 +1,74 @@
+// frappe.query_reports["URD PURCHASE REPORT"] = {
+
+//     filters: [
+//         {
+//             fieldname: "from_date",
+//             label: "From Date",
+//             fieldtype: "Date",
+//             default: frappe.datetime.get_today(),
+//             reqd: 1
+//         },
+//         {
+//             fieldname: "to_date",
+//             label: "To Date",
+//             fieldtype: "Date",
+//             default: frappe.datetime.get_today(),
+//             reqd: 1
+//         },
+//         {
+//             fieldname: "client_name",
+//             label: "Customer",
+//             fieldtype: "Link",
+//             options: "Customer"
+//         }
+//     ],
+
+//     formatter: function(value, row, column, data, default_formatter) {
+//         value = default_formatter(value, row, column, data);
+
+//         // Highlight total row
+//         if (data && data.customer_id === "TOTAL") {
+//             value = `<b>${value}</b>`;
+//         }
+
+//         return value;
+//     }
+// };frappe.query_reports["URD PURCHASE REPORT"] = {
+
+//     filters: [
+//         {
+//             fieldname: "from_date",
+//             label: "From Date",
+//             fieldtype: "Date",
+//             default: frappe.datetime.get_today(),
+//             reqd: 1
+//         },
+//         {
+//             fieldname: "to_date",
+//             label: "To Date",
+//             fieldtype: "Date",
+//             default: frappe.datetime.get_today(),
+//             reqd: 1
+//         },
+//         {
+//             fieldname: "client_name",
+//             label: "Customer",
+//             fieldtype: "Link",
+//             options: "Customer"
+//         }
+//     ],
+
+//     formatter: function(value, row, column, data, default_formatter) {
+//         value = default_formatter(value, row, column, data);
+
+//         // Highlight total row
+//         if (data && data.customer_id === "TOTAL") {
+//             value = `<b>${value}</b>`;
+//         }
+
+//         return value;
+//     }
+// };
 frappe.query_reports["URD PURCHASE REPORT"] = {
 
     filters: [
